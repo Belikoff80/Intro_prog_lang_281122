fe4fd9f6b73cf2911de2dad0370ef3fbe48f2f48
Belikoff80 <maxim.belikoff@gmail.com>
2022-11-29 15:55:29 +0300

    add Example006_Condition_IfElse

---
+++ Example006_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользоваеля: ");
+string username = Console.ReadLine();
+
+If(username.ToLower() == "маша");
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else;
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
